452fc5e943855b237219fe12281a7b17260a0d82
Juza <juanlozadahe@gmail.com>
2022-03-27 12:37:48 -0500

    first commit - 'work on progress' basic page

---
+++ src/Components/App/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import '../Content/Content'
+
+import Content from '../Content/Content';
+import Illustration from '../Illustration/Illustration';
+
+function App() {
+  return (
+    <div className="App">
+      <Content/>
+      <Illustration/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Illustration/Illustration.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Illustration.css';
+
+export default class Illustration extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
